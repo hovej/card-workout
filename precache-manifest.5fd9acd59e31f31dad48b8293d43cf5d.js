a442fc759fc99873f1e6fa25b11799afd34fc8e6
Justin Hove <hovej88@gmail.com>
2020-06-04 19:20:47 -0500

    Updates

---
+++ precache-manifest.5fd9acd59e31f31dad48b8293d43cf5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29b954246ba06e43a90f6f52cf9b4b10",
+    "url": "/card-workout/index.html"
+  },
+  {
+    "revision": "bef70c491edd2f4c2a8f",
+    "url": "/card-workout/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "065fd57134c3e5c5f30a",
+    "url": "/card-workout/static/js/2.a3356d63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/card-workout/static/js/2.a3356d63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bef70c491edd2f4c2a8f",
+    "url": "/card-workout/static/js/main.4e21adea.chunk.js"
+  },
+  {
+    "revision": "2648b850e083cdbf7aba",
+    "url": "/card-workout/static/js/runtime-main.27317aac.js"
+  }
+]);
